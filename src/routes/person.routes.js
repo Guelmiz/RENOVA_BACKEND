8ccb1fad0d4d298decf3620e1d6616a750040794
Miguel <angelsoza53@gmail.com>
2025-11-09 09:06:31 -0600

    Se termino el crud de personas

--- src/routes/person.routes.js
+++ src/routes/person.routes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import { getPersonas, getPersona, addPersona } from '../controllers/person.controllers.js';
+import { getPersonas, getPersona, addPersona, updatePersona,deletePersona } from '../controllers/person.controllers.js';
 const router = Router();
 
 router.get('/personas',getPersonas);
 router.get('/personas',getPersona);
 router.post('/personas',addPersona);
+router.put('/personas/:id',updatePersona);  
+router.delete('/personas/:id',deletePersona);   
 export default router;
