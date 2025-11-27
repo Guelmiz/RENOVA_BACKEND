7b4a84ea463f0403cfe0417249ff6e0a633fa412
Miguel <angelsoza53@gmail.com>
2025-11-26 21:23:41 -0600

    configuracion de cors

--- src/index.js
+++ src/index.js
@@ -20,13 +20,11 @@ const app = express();
 const whitelist = ['process.env.FRONTEND_URL, http://localhost:3000'];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('No permitido por CORS'))
-    }
-  }
+  origin: [
+    "http://localhost:3000",
+    "https://renova-frontend-a5su.vercel.app" 
+  ],
+  credentials: true
 }));
 
 const __dirname = process.cwd();
